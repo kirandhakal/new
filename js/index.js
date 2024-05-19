d981ea5af6aee79431f2049e0308846bffe7e591
kirandhakal <116267720+kirandhakal@users.noreply.github.com>
2024-05-19 12:54:14 +0545

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,92 @@
+// clock start
+const displayTime = document.querySelector(".display-time");
+// Time
+function showTime() {
+  let time = new Date();
+  displayTime.innerText = time.toLocaleTimeString("en-US", { hour12: false });
+  setTimeout(showTime, 1000);
+}
+
+showTime();
+
+// Date
+function updateDate() {
+  let today = new Date();
+
+  // return number
+  let dayName = today.getDay(),
+    dayNum = today.getDate(),
+    month = today.getMonth(),
+    year = today.getFullYear();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const dayWeek = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+  ];
+  
+    // Get the suffix for the day number
+    function getDaySuffix(day) {
+      if (day >= 11 && day <= 13) {
+        return "th";
+      }
+      switch (day % 10) {
+        case 1:
+          return "st";
+        case 2:
+          return "nd";
+        case 3:
+          return "rd";
+        default:
+          return "th";
+      }
+    }
+
+  // value -> ID of the html element
+  const IDCollection = ["day", "daynum", "month", "year"];
+  // return value array with number as a index
+  const val = [dayWeek[dayName], dayNum + getDaySuffix(dayNum), months[month], year];
+  for (let i = 0; i < IDCollection.length; i++) {
+    document.getElementById(IDCollection[i]).firstChild.nodeValue = val[i];
+  }
+}
+
+updateDate();
+//clock ends
+
+//animations
+const Observer =new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+
+    }else{
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenelements =document.querySelectorAll('.skil');
+hiddenelements.forEach(
+  (el)=> Observer.observe(el)
+);
+
+//
