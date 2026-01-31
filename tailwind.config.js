1b6e6ccc9751128fb8eb261d3d1271fac610a5a4
kirandhakal <kirandhakal715@gmail.com>
2026-01-31 20:57:32 +0545

    fix-build issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./src/**/*.{js,jsx,ts,tsx}",
-  "./src/components/**/*.{js,jsx,ts,tsx}"
-];
-export const theme = {
-  extend: {
-    animation: {
-      'spin-slow': 'spin 6s linear infinite',
-      'spin-slower': 'spin 12s linear infinite',
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 6s linear infinite',
+        'spin-slower': 'spin 12s linear infinite',
+      },
     },
   },
-};
-export const plugins = [];
+  plugins: [],
+}
