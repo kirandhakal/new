d83c7f2de0ffe5361639a4ff359155e58a276907
kirandhakal <kirandhakal715@gmail.com>
2026-05-23 23:24:36 +0545

    Remove generated type definition files for app pages and services

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,18 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-c8441f2d519541e3.js","static/chunks/fd9d1056-cce117dc4e21e608.js","static/chunks/117-2593ac4cd608cb7b.js","static/chunks/main-app-0c6f5a09664f4795.js"],pages:{"/_app":["static/chunks/webpack-c8441f2d519541e3.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-ec97da1174e9d920.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-c8441f2d519541e3.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-ec97da1174e9d920.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
